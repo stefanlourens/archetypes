7a775b255cca2f599f4135e801fa7591eef02b11
Stefan Lourens <stefan.lourens@gmail.com>
2013-03-04 19:05:58 +0200

    Remove ResourceLoader

--- resteasy-guice-service/src/main/resources/archetype-resources/src/main/java/GuiceModule.java
+++ resteasy-guice-service/src/main/resources/archetype-resources/src/main/java/GuiceModule.java
@@ -6,12 +6,14 @@ package ${package};
 import com.google.inject.AbstractModule;
 import ${package}.greeter.DefaultGreeter;
 import ${package}.greeter.Greeter;
+import ${package}.resources.EchoResource;
 import ${package}.resources.HelloResource;
 
 public class GuiceModule extends AbstractModule {
 
     @Override
     protected void configure() {
+        bind(EchoResource.class);
         bind(HelloResource.class);
         bind(Greeter.class).to(DefaultGreeter.class);
     }
